cb3e9abdab3e6d22c0e83548dcf3e5a56efa4d31
Rafael Stauffer <rafael.stauffer@gmx.ch>
2022-02-02 22:09:41 +0100

    Added crude main engine

--- src/main/java/Vostok.java
+++ src/main/java/Vostok.java
@@ -1,9 +1,23 @@
 package com.opcode.fx_test;
 
-class Vostok {
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Vostok extends Thread {
     Vector3d position;
     Vector3d heading;
     Vector3d velocity;
+
+    double totalTime;
+
+    // not a real rocket equation but works for the moment
+    boolean mainRocketIgnited = false;
+    double deltaVStorage = 155;
+    double deltaVSecond = 1.55;
+
+    Lock modificationLock;
 
     static final double gravitationalConstant = 6.67430 * Math.pow(10,-11);
     static final double massEarth = 5.972e24;
@@ -15,14 +29,33 @@ class Vostok {
 
     static final double dragArea = 4.15;
     static final double dragCoefficient = 0.5;
+    
+    static final long UPDATE_SLEEP_TIME_MS = 1000;
 
-    double totalTime;
+    public void run() {
+        long start = System.currentTimeMillis();
+        long finish = 0;
+        while (!this.isInterrupted()) {
+            try {
+                Thread.sleep(UPDATE_SLEEP_TIME_MS);
+                finish = System.currentTimeMillis();
+                if (this.update((double)((finish - start) / UPDATE_SLEEP_TIME_MS))) {
+                    this.interrupt(); // we crashed, no need to update
+                }
+                start = System.currentTimeMillis();
+            }
+            catch (Exception e) {
+                this.interrupt();
+            }
+        }
+    }
 
     Vostok(Vector3d position, Vector3d heading, Vector3d velocity) {
         this.position = position;
         this.heading = heading;
         this.velocity = velocity;
         this.totalTime = 0;
+        modificationLock = new ReentrantLock();
     }
 
     private Vector3d getEarthDirection() {
@@ -69,21 +102,45 @@ class Vostok {
         return forceNewton / massKg;
     }
 
-    public boolean update(double deltaTime) {
-        this.totalTime += deltaTime;
+    public void igniteMainRocket() {
+        this.modificationLock.lock();
+        this.mainRocketIgnited = true;
+        System.out.println("firing main rocket");
+        this.modificationLock.unlock();
+    }
 
+    public boolean update(double deltaTime) {
+        this.modificationLock.lock();
+        this.totalTime += deltaTime;
         double gravity = getVelocityFromForce(getGravitationalForce(massEarth, massVostok, getDistanceToEarthCenter(this.position)), massVostok);
-        //System.out.println("g: " + gravity);
         this.velocity = this.velocity.add(this.getEarthDirection().multiply(gravity).multiply(deltaTime));
 
         double orbitDecayDrag = getVelocityFromForce(getDragForce(getAirDensityAtAltitude(getDistanceToEarthCenter(this.position)), dragArea, this.velocity.getLength(), dragCoefficient), massVostok);
         if ((!Double.isNaN(orbitDecayDrag)) && (!Double.isInfinite(orbitDecayDrag)) && (orbitDecayDrag != 0.0)) {
-            //System.out.println("timestamp " + this.totalTime + "\nheigth: " + getHeightOverEquator() + "\ndensity: "+ getAirDensityAtAltitude(getDistanceToEarthCenter(this.position)) + "\ndrag: " + orbitDecayDrag);
             this.velocity = this.velocity.subtract(this.velocity.normalize().multiply(orbitDecayDrag).multiply(deltaTime));
+        }
+
+        if (this.mainRocketIgnited) {
+            if (deltaVStorage > 0) {
+                double burnTime = deltaTime;
+                if (deltaVStorage / deltaVSecond >= deltaTime) {
+                    deltaVStorage -= deltaVSecond * deltaTime;
+                } else {
+                    burnTime = deltaVStorage / deltaVSecond;
+                    deltaVStorage = 0;
+                }
+                System.out.println("main rocket burn: " + burnTime);
+                this.velocity = this.velocity.add(this.heading.normalize().multiply(deltaVSecond).multiply(burnTime));
+            }
         }
 
         Vector3d deltaVelocity = this.velocity.multiply(deltaTime);
         this.position = this.position.add(deltaVelocity);
+
+        System.out.println("timestamp: " + this.totalTime);
+        System.out.println("pos " + this.position.getX() + " " + this.position.getY() + " " + this.position.getZ());
+
+        this.modificationLock.unlock();
 
         return positionInsideEarth(this.position);
     }
